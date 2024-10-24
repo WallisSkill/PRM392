dd4504bcba13d89acf4691602bfd680bb503d72e
hoangnxhe171096 <hoangnxhe171096@fpt.edu.vn>
2024-10-24 23:58:28 +0700

    Update ViewProfile + ResetPassword

--- app/src/main/java/com/example/myapplication/Login/ViewProfileActivity.java
+++ app/src/main/java/com/example/myapplication/Login/ViewProfileActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication.Login;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.Homepage.AdminHomepageActivity;
+import com.example.myapplication.Homepage.CustomerHomepageActivity;
 import com.example.myapplication.R;
 
 public class ViewProfileActivity extends AppCompatActivity {
+    TextView txtName, txtEmail, txtPhone, txtAddress;
+    Button btnBack, btnResetPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,42 @@ public class ViewProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtName = findViewById(R.id.txtName);
+        txtAddress = findViewById(R.id.txtAddress);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPhone = findViewById(R.id.txtPhone);
+        btnBack = findViewById(R.id.btnBack);
+        btnResetPass = findViewById(R.id.btnResetPass);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("id", -1);
+        String username = sharedPreferences.getString("username", null);
+        String email = sharedPreferences.getString("email", null);
+        String phone = sharedPreferences.getString("phone", null);
+        String address = sharedPreferences.getString("address", null);
+        boolean role = sharedPreferences.getBoolean("role", false);
+        txtName.setText(username);
+        txtPhone.setText(phone);
+        txtEmail.setText(email);
+        txtAddress.setText(address);
+
+
+        btnBack.setOnClickListener(v -> {
+            Intent i;
+            if (role) {
+                i = new Intent(this, AdminHomepageActivity.class);
+                startActivity(i);
+            } else {
+                i = new Intent(this, CustomerHomepageActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnResetPass.setOnClickListener(v -> {
+            Intent i = new Intent(this, ResetPasswordActivity.class);
+            startActivity(i);
+        });
+
+
+
     }
 }
